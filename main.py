006502ab0cbcd12f1bdc33e1a2ae667b4e0861ba
Xiancong <xiancong88@gmail.com>
2025-05-29 22:52:26 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from Generator.secret import get_secret
+import onetimepass as otp
+import pyperclip
+
+def main():
+    secret = get_secret()
+    my_otp = str(otp.get_totp(my_secret))
+    my_otp = my_otp.rjust(6,'0') # if otp is less than 6 digits that means that the first x digits are 0
+    pyperclip.copy(my_otp)
+
+if __name__ == "__main__":
+    main()
