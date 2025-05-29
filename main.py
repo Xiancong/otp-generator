a62b193e01d3e9d07fb1cb1f6f0f84c79dafe8ff
Xiancong <xiancong88@gmail.com>
2025-05-29 22:55:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     secret = get_secret()
     my_otp = str(otp.get_totp(my_secret))
     my_otp = my_otp.rjust(6,'0') # if otp is less than 6 digits that means that the first x digits are 0
-    pyperclip.copy(my_otp)
+    pyperclip.copy(my_otp) # Copy to clipboard
 
 if __name__ == "__main__":
     main()
